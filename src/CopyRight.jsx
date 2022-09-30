7714392c179f0c3add9a0cfdbd3558c860914b39
Akinwande001 <111493479+Akinwande001@users.noreply.github.com>
2022-09-30 05:17:16 -1200

    Update CopyRight.jsx

--- src/CopyRight.jsx
+++ src/CopyRight.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
  function CopyRight(){
 
@@ -15,22 +14,4 @@ import React from "react";
     
  )}
 
-=======
-import React from "react";
- function CopyRight(){
-
-    const currentYear =  new Date().getFullYear();
-    const conStyle ={
-        textAlign :"center",
-        fontSize : "1rem",
-        paddingBottom : "20px",
-        colors: "grey"
-    }
-    return(
-    
-    <p style={conStyle}>Freestyle Project {currentYear}</p>
-    
- )}
-
->>>>>>> 0ba6be3a16a6452ef83cedc3a991c27e6f8bf719
  export default CopyRight
